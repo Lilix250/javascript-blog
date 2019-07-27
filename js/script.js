67581601e8c40c0b2736c25d092596098af6e2d7
Liliana Czeredys <liliana.czeredys@gmail.com>
2019-07-27 21:18:32 +0200

    finished site

--- js/script.js
+++ js/script.js
@@ -40,8 +40,31 @@ const titleClickHandler = function(event){
 }
 
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles){
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        const linkHTML = '<li><a href="#' + articleId  + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+        html += linkHTML;
+    }
+   
+    titleList.insertAdjacentHTML('beforeend', html);
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }  
 }
+
+generateTitleLinks();
